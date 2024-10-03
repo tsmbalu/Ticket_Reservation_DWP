38c4d9eaa76595ea793f9c19491695767e223b57
Balu <balu.aravi@gmail.com>
2024-10-03 13:02:33 +0100

    Added Java Doc Comment

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketPricingStrategyFactory.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketPricingStrategyFactory.java
@@ -3,7 +3,18 @@ package uk.gov.dwp.uc.pairtest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
+/**
+ * This class is the factory class for Ticket pricing strategy.
+ * Based on the ticket type corresponding pricing strategy will be applied
+ */
 public class TicketPricingStrategyFactory {
+
+    /**
+     * Factory method for pricing strategy
+     *
+     * @param type Ticket Type
+     * @return
+     */
     public static TicketPricingStrategy getStrategy(TicketTypeRequest.Type type) {
         if (type == null){
             throw new IllegalArgumentException("Unknown ticket type");
--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -8,6 +8,11 @@ import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.dto.TicketOrderSummary;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
+/**
+ * This class provides ticket booking services for various types of tickets
+ * such as adult, child, and infant tickets. It validates ticket requests
+ * based on business rules and ensures proper seat reservations and payments.
+ */
 public class TicketServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
@@ -16,7 +21,6 @@ public class TicketServiceImpl implements TicketService {
     private static final int MAX_TICKETS_ALLOWED = 25;
     private SeatReservationService seatReservationService;
     private TicketPaymentService ticketPaymentService;
-
 
     public TicketServiceImpl(){
         this.seatReservationService = new SeatReservationServiceImpl();
@@ -28,6 +32,13 @@ public class TicketServiceImpl implements TicketService {
         this.ticketPaymentService = ticketPaymentService;
     }
 
+    /**
+     * This method is to purchase tickets
+     *
+     * @param accountId account id of the app
+     * @param ticketTypeRequests ticket request with type of tickets and quantity of tickets
+     * @throws InvalidPurchaseException
+     */
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
 
@@ -52,12 +63,26 @@ public class TicketServiceImpl implements TicketService {
         }
     }
 
+    /**
+     * This method is to validate whether the account is valid or not.
+     * Assumption: Account ID is valid if it is greater than 0
+     *
+     * @param accountId
+     * @throws InvalidPurchaseException
+     */
     private void validateAccount(Long accountId) throws InvalidPurchaseException {
         if (accountId == null || accountId <= 0) {
             throw new InvalidPurchaseException("Invalid account ID.");
         }
     }
 
+    /**
+     * This method is to create ticket order summary. Calculate the total price of the ticket order
+     * and total number of tickets in the request
+     *
+     * @param ticketTypeRequests List of tickettypes in the request
+     * @return
+     */
     private TicketOrderSummary createOrderSummary(TicketTypeRequest... ticketTypeRequests) {
         int totalTicketPrice = 0;
         int totalNumberOfTicket = 0;
